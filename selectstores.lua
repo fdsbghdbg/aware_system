85a632dc885c3ab4af22c6227f3f49374545f49a
AngerminecraftYT <83855262+AngerminecraftYT@users.noreply.github.com>
2023-12-30 19:31:13 +0100

    SelectStores Handler

---
+++ selectstores.lua
@@ -0,0 +1,4 @@
+--[[ MaintainceNo - Change to MaintainceYes if maintaince ]]--
+
+warn("[select.] Thanks for supporting us, our store system is loading....")
+-- load other stuff which handle payment etc.
