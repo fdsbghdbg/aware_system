9598ca140f27a432ba2c543a5faeb7fcddd9ca35
asdffeyy <145255707+asdffeyy@users.noreply.github.com>
2023-12-31 12:54:53 +0100

    Update selectpos.lua

--- selectpos.lua
+++ selectpos.lua
@@ -2,4 +2,5 @@
 print("[select.] System connected successfully.")
 print("insert cash or select other payment type")
 print(gui or "Gui dont exist in main script. Define somehow.")
-gui.ok.Visible=true
+gui.ok.Visible = true
+print("I'm Select the gigachad")
