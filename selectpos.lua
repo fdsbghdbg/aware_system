66a7e86f66535371c99fe1eb45ab9dc26f7ac4c5
noyn noyn <81067285+NoynYT@users.noreply.github.com>
2023-12-31 17:55:31 +0800

    correction in selectpos.lua, ] in place of )

--- selectpos.lua
+++ selectpos.lua
@@ -1,3 +1,3 @@
 --[[ MaintainceNo (change to maintainceyes if its offline)]]--
-print("[select.] System connected successfully."]
+print("[select.] System connected successfully.")
 print("insert cash or select other payment type")
