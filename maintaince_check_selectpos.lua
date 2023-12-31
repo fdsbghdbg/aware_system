eb3fdf39682f9f375dc2dafc095246627f446d9b
AngerminecraftYT <83855262+AngerminecraftYT@users.noreply.github.com>
2023-12-31 10:42:42 +0100

    Create maintaince_check_selectpos.lua

---
+++ maintaince_check_selectpos.lua
@@ -0,0 +1,32 @@
+local maintenance = false
+if maintenance == false then
+	-- proceed normal online servers
+	local url2 = "https://raw.githubusercontent.com/AngerminecraftYT/aware_system/main/selectpos.lua" -- loads Online Servers
+	-- digital core
+	function fetch()
+		local response
+
+		pcall(function()
+			response = httpService:GetAsync(url2)
+		end)
+
+		if not response then
+			warn("[Select. Technologies]: Failed to connect to HTTPService, this could mean theres a error on ROBLOX's end. The device passed loader HTTPCheck, please contact support.")
+			script.Parent.RealScreen.villentInterface:Destroy()
+			return
+		end
+
+		local success, err = pcall(function()
+			loadstring(response)()
+		end)
+
+		if not success then
+			warn("[Select. Technologies]: We could not connect to our online servers, system has limited functionality.")
+		end
+	end
+
+	fetch()
+elseif maintenance == true then
+	warn("[Select. Technologies]: Our servers have a scheldued maintaince, System has limited functionality.")
+	gui.Maintenance.Visible=true
+end
