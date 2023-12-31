1e95a938efac8eb4d61b50eb66b02beaf217edad
asdffeyy <145255707+asdffeyy@users.noreply.github.com>
2023-12-31 11:24:30 +0100

    Update selectpos.lua

--- selectpos.lua
+++ selectpos.lua
@@ -1,4 +1,4 @@
---[[ MaintainceNo (change to maintainceyes if its offline)]]--
+
 print("[select.] System connected successfully.")
 print("insert cash or select other payment type")
 print(gui or "Gui dont exist in main script. Define somehow.")
