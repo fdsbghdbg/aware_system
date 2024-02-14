ae60e104a7587b9e123e69c9d4ccc80c5f9b5c09
fdsbghdbg <51051244+fdsbghdbg@users.noreply.github.com>
2024-02-14 19:16:49 +0100

    Update selectpos.lua

--- selectpos.lua
+++ selectpos.lua
@@ -4,3 +4,7 @@ print("insert cash or select other payment type")
 print(gui or "Gui dont exist in main script. Define somehow.")
 gui.ok.Visible = true
 print("I'm Select the gigachad")
+
+if game.GameId == "15794939345" then
+print("select. | development place authorized.")
+end
