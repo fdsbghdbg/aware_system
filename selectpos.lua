c9c36930a6cbab6a248cd27b05f6e20246b7ccd1
tmrx <51051244+tmrxdev@users.noreply.github.com>
2024-08-01 10:20:10 +0200

    Update selectpos.lua

--- selectpos.lua
+++ selectpos.lua
@@ -3,8 +3,8 @@ print("[select.] System connected successfully.")
 print("insert cash or select other payment type")
 print(gui or "Gui dont exist in main script. Define somehow.")
 gui.ok.Visible = true
-print("I'm Select the gigachad")
+print("I'm select the gigachad")
 
 if game.GameId == "15794939345" then
-print("select. | development place authorized.")
+print("granting access")
 end
